a880cc740f2645307b1ae01dfbf30ecf513f78eb
western85 <84554545+western85@users.noreply.github.com>
2022-07-28 17:59:30 -0700

    add index.html file

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+
 app.get('/', (req, res) => {
-    res.send('Sup');
+    res.render('home')
 })
 
-//
-
-app.listen(3030, () => {
-    console.log('Serving on port 3030')
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
 });
