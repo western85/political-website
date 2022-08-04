52bec67a344f8850d68d0862b609f0b48826479b
western85 <84554545+western85@users.noreply.github.com>
2022-08-04 16:24:01 -0700

    added photos and an info page

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get("/info", (req, res) => {
+  console.log("req for info received");
+  res.render("pages/info");
+});
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
+  console.log("Serving on port 3000");
 });
